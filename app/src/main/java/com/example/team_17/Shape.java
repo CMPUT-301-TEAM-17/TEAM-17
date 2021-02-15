5839e8dfddaf8e78b814765737a8415078c11e24
qasim-tech <memon.qasim786@gmail.com>
2021-02-15 17:30:07 +0400

    added a color

--- app/src/main/java/com/example/team_17/Shape.java
+++ app/src/main/java/com/example/team_17/Shape.java
@@ -3,5 +3,6 @@ package com.example.team_17;
 public abstract class Shape {
     protected int x;
     protected int y;
+    protected String color = "blue";
     protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
